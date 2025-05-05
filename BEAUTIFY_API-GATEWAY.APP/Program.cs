b917ac44c3e4a9ce7ca236c921d66baee7b2dbf3
bentran1vn <tan182205@gmail.com>
2025-05-05 15:36:21 +0700

    Run again

--- BEAUTIFY_API-GATEWAY.APP/Program.cs
+++ BEAUTIFY_API-GATEWAY.APP/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddJwtAuthenticationApiGateway(builder.Configuration);
 builder.Services.AddReverseProxyApiGateway(builder.Configuration);
 
 var app = builder.Build();
-
+// Configure the HTTP request pipeline.
 app.UseAuthentication();
 app.UseAuthorization();
 
